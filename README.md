9b3d5702fea79d9650fa3d76b1dd2b7c4cd0c889
shch989 <tankpop11@gmail.com>
2023-10-22 01:07:40 +0900

    contracts 및 script 코드 작성

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Sample Hardhat Project
+```
+$ npm init -y
+```
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+```
+$ npm install hardhat
+```
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+```
+$ npx hardhat
 ```
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// Hardhat 라이브러리를 가져옵니다.
+const hre = require('hardhat')
 
+// 비동기 함수 main을 정의합니다.
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // 'chai' 스마트 계약의 팩토리(클래스)를 가져옵니다.
+  const Chai = await hre.ethers.getContractFactory('chai')
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // 'chai' 팩토리를 사용하여 새로운 'chai' 스마트 계약 인스턴스를 배포합니다.
+  const chai = await Chai.deploy()
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // 스마트 계약이 배포되었을 때까지 대기합니다.
+  await chai.deployed()
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // 배포된 스마트 계약의 주소를 콘솔에 출력합니다.
+  console.log('Deployed contract address:', `${chai.address}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+// main 함수를 호출하여 스마트 계약을 배포하고 주소를 출력합니다.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  // 코드 실행 중에 오류가 발생하면 오류를 콘솔에 출력하고 프로세스 종료 코드를 1로 설정하여 오류 상태를 표시합니다.
+  console.error(error)
+  process.exitCode = 1
+})
